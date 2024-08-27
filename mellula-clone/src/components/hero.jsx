a36e928b2bbc6e06d663056b91eda9da749cc62e
noluzar <noluthandosamantha.ns@gmail.com>
2024-08-27 13:46:20 +0200

    final commit

--- mellula-clone/src/components/hero.jsx
+++ mellula-clone/src/components/hero.jsx
@@ -3,12 +3,15 @@
 
 export const Hero = () => {
   return (
-    <div className="top-0 absolute w-full z-0 flex justify-center items-center flex-col" style={{backgroundImage:`url('./22.jpg')`,
+    <div className="top-0 overflow-x-hidden absolute bg-cover w-full h-full z-0 flex justify-center items-center flex-col" style={{backgroundImage:`url('./22.jpg')`,
         height: '100vh',
         backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center'
+        backgroundPosition: 'center',
+        backgroundSize: ""
+        
     }}>
-        <div className="text-white text-[67px] w-[600px] text-center pb-[17px] font-semibold">FOR COLOURFUL STEPS IN LIFE</div>
+        <div className="text-white text-[67px] text-center  font-semibold">FOR COLOURFUL </div>
+        <div className="text-white text-[67px] text-center pb-[17px] font-semibold">STEPS IN LIFE</div>
         <button className="bg-[#0DF8AD]   px-[34px] py-[21px] rounded-full text-[18px] text-white p-[17px] font-semibold">VISIT OUR SHOP</button>
     </div>
   )
--- mellula-clone/src/components/navbar.jsx
+++ mellula-clone/src/components/navbar.jsx
@@ -1,18 +1,50 @@
-// import React from 'react'
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="text-white top-0 left-0 bg-transparent z-10 fixed w-full">
-        <nav className="flex justify-between w-full pt-[40px] px-[50px] items-center">
-            <div>
-                <img src={'./logo2.png'} className="w-[152px] h-[52px]"></img>
-            </div>
-            <ul className="list-none flex space-x-4 text-[15px] font-semibold">
-                <li><a href="#">SHOP</a></li>
-                <li><a href="#">ABOUT</a></li>
-            </ul>
-            <div>Icon</div>
-        </nav>
+    <div className="text-white top-0 left-0 bg-[#002D5A] z-10 fixed w-full md:bg-transparent">
+      <nav className="flex justify-between w-full py-[8px] h-[78px] px-[12px] md:pt-[40px] md:px-[50px] items-center">
+        <div>
+          <img src={'./logo2.png'} className="md:w-[152px] md:h-[52px] w-[69px] h-[24px]" alt="Logo" />
+        </div>
+        <ul className="list-none flex md:space-x-4 text-[15px] font-semibold">
+          <li><a href="#" className="hidden md:flex">SHOP</a></li>
+          <li><a href="#" className="hidden md:flex">ABOUT</a></li>
+          <MdOutlineShoppingCart className="md:h-[35px] md:w-[35px] w-[30px] h-[30px] md:hidden" />
+        </ul>
+        <div className="flex items-center">
+          <MdOutlineShoppingCart className="md:h-[35px] md:w-[35px] w-[30px] h-[30px] hidden md:flex" />
+          <button onClick={toggleMenu} className="md:hidden ml-4">
+            {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
+          </button>
+        </div>
+      </nav>
+
+      {isOpen && (
+              <div className="md:hidden bg-white w-full h-[100vh] top-0 z-10 absolute">
+                  <div className="flex flex-col items-start px-3 py-4 space-y-3">
+                      <button onClick={toggleMenu} className="md:hidden ml-1">
+                          {isOpen ? <FiX size={24} className="text-black" /> : <h1></h1>}
+                      </button>
+                      <div className="flex-col flex ml-10  font-semibold text-[21px]">
+                      <a href="/" className="text-black pb-[13px] hover:text-gray-600">
+                          SHOP
+                      </a>
+                      <a href="/" className="text-black hover:text-gray-600">
+                          ABOUT
+                      </a>
+                      </div>
+                  </div>
+              </div>
+      )}
     </div>
-  )
-}
+  );
+};
